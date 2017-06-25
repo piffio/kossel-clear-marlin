d9913963b3f3d6db1d888600ccd806bb1a0a3e21
Sergio Visinoni <piffio@piffio.org>
2017-06-25 20:59:34 +0200

    Change thermistor type for E3DV6
    Configure the correct type of thermistore for the new
    E3DV6 nozzle

--- Configuration.h
+++ Configuration.h
@@ -133,7 +133,8 @@
 // 52 is 200k thermistor - ATC Semitec 204GT-2 (1k pullup)
 // 55 is 100k thermistor - ATC Semitec 104GT-2 (Used in ParCan) (1k pullup)
 
-#define TEMP_SENSOR_0 1
+// #define TEMP_SENSOR_0 1 // For the original J-Head
+#define TEMP_SENSOR_0 5 // E3DV6
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_BED 1
